3e979f281997126c660f4ca06997b852106ef033
Christie Speich <christiespeich@gmail.com>
2024-12-13 11:42:43 -0500

    part 2

--- 2024/01/index.php
+++ 2024/01/index.php
@@ -7,8 +7,8 @@ $list_2 = array();
 
 foreach ( $input as $line ) {
 	$list_values = preg_split("/ +/", $line);
-	$list_1[] = $list_values[0];
-	$list_2[] = $list_values[1];
+	$list_1[] = trim($list_values[0]);
+	$list_2[] = trim($list_values[1]);
 }
 
 sort($list_1);
@@ -21,3 +21,13 @@ for( $x=0; $x<count($list_1); $x++ ) {
 
 echo $difference;
 echo "\n";
+
+$list2_count = array_count_values($list_2);
+$similarity = 0;
+foreach ( $list_1 as $value ) {
+	if( array_key_exists($value, $list2_count) ) {
+		$similarity += $list2_count[$value] * $value;
+	}
+}
+echo $similarity . "\n";
+
